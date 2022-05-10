9bd9d12a7f9364cc8b5cfa772c8999c730d3d53a
Roni Bilkevich <71309493+Ronibil@users.noreply.github.com>
2022-05-10 16:13:06 +0300

    Added super student ref from classcard

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,16 @@ import FCStudentHomePage from "./Pages/FCStudentHomePage";
 import FCTypeOfUser from "./Pages/FCTypeOfUser";
 import FCSearchClassesPage from "./Pages/FCSearchClassesPage";
 import "bootstrap/dist/css/bootstrap.min.css";
-import FCStudentRequestPage from './Pages/FCStudentRequestPage';
-import FCSuperStudentRequestPage from './Pages/FCSuperStudentRequestPage.jsx';
-import FCSuperStudentRequestPage2 from './Pages/FCSuperStudentRequestPage2.jsx';
+import FCStudentRequestPage from "./Pages/FCStudentRequestPage";
+import FCSuperStudentRequestPage from "./Pages/FCSuperStudentRequestPage.jsx";
+import FCSuperStudentRequestPage2 from "./Pages/FCSuperStudentRequestPage2.jsx";
 import FCCreateNewClass from "./FuncionlComps/FCCreateNewClass";
 import FCUpdateProfileSuper from "./Pages/FCUpdateProfileSuper";
 import FCHistoryClassSuper from "./Pages/FCHistoryClassSuper";
 import FCFutreClasses from "./Pages/FCFutreClasses";
 import CCAdminHomePage from "./Pages/CCAdminHomePage";
 import CCHandleRequest from "./Pages/CCHandleRequest";
-
+import FCFormSuperDetails from "./FuncionlComps/FCFormSuperDetails";
 
 export default function App() {
   return (
@@ -25,18 +25,28 @@ export default function App() {
         <Route path="/adminHomePage" element={<CCAdminHomePage />} />
         <Route path="/reqDetails" element={<CCHandleRequest />} />
         <Route path="/searchClassesPage" element={<FCSearchClassesPage />} />
-        <Route path='/' element={<FCLoginPage />} />
-        <Route path='/StudentRequestPage' element={<FCStudentRequestPage />} />
-        <Route path='/SuperStudentRequestPage' element={<FCSuperStudentRequestPage />} />
-        <Route path='/SuperStudentRequestPage2' element={<FCSuperStudentRequestPage2 />} />
-        <Route path='/studentHomePage' element={<FCStudentHomePage />} />
-        <Route path='/superHomePage' element={<FCSuperHomePage />} />
-        <Route path='/typeOfUser' element={<FCTypeOfUser />} />
-        <Route path='/CreateNewClass' element={<FCCreateNewClass />} />
-        <Route path='/UpdateProfileSuper' element={<FCUpdateProfileSuper />} />
-        <Route path='/HistoryClassSuper' element={<FCHistoryClassSuper />} />
-        <Route path='/FutreClasses' element={<FCFutreClasses />} />
+        <Route path="/" element={<FCLoginPage />} />
+        <Route path="/StudentRequestPage" element={<FCStudentRequestPage />} />
+        <Route
+          path="/SuperStudentRequestPage"
+          element={<FCSuperStudentRequestPage />}
+        />
+        <Route
+          path="/SuperStudentRequestPage2"
+          element={<FCSuperStudentRequestPage2 />}
+        />
+        <Route path="/studentHomePage" element={<FCStudentHomePage />} />
+        <Route path="/superHomePage" element={<FCSuperHomePage />} />
+        <Route path="/typeOfUser" element={<FCTypeOfUser />} />
+        <Route path="/CreateNewClass" element={<FCCreateNewClass />} />
+        <Route path="/UpdateProfileSuper" element={<FCUpdateProfileSuper />} />
+        <Route path="/HistoryClassSuper" element={<FCHistoryClassSuper />} />
+        <Route path="/FutreClasses" element={<FCFutreClasses />} />
+        <Route
+          path="/FCFormSuperDetails"
+          element={<FCFormSuperDetails />}
+        ></Route>
       </Routes>
     </div>
   );
-};
+}
--- src/FuncionlComps/FCClassCard.jsx
+++ src/FuncionlComps/FCClassCard.jsx
@@ -1,90 +1,206 @@
 import React from "react";
 import { Card, Row, Col, Button } from "react-bootstrap";
+import { Navigate, NavigationType, useNavigate } from "react-router-dom";
 
 export default function FCClassCard({ classToCard, type, ShowModaAreYouSure }) {
-  if (type === "history") {
-    return (
-      <>
-        <Card>
-          <Card.Body>
-            <Row>
-              <Col xs={12}>
-                <b>שם השיעור:</b> {classToCard.ClassName}
-                <br />
-                <b>תאריך:</b>{" "}
-                {new Date(classToCard.ClassDate).toLocaleDateString("en-GB")}{" "}
-                <br />
-                <b>שעת התחלה:</b> {classToCard.StartTime}
-              </Col>
-            </Row>
-          </Card.Body>
-        </Card>
-      </>
-    );
-  } else if (type === "studentFutre") {
-    return (
-      <>
-        <Card>
-          <Card.Body>
-            <Row>
-              <Col xs={2}>
-                <Button
-                  onClick={() => ShowModaAreYouSure(classToCard)}
-                  variant="outline-danger"
-                >
-                  מחיקה
-                </Button>
-              </Col>
-              <Col xs={2}>
-                <Button
-                  onClick={() => ShowModaAreYouSure(classToCard)}
-                  variant="outline-primary"
-                >
-                  לצ'אט
-                </Button>
-              </Col>
-              <Col xs={8}>
-                <b>שם השיעור:</b> {classToCard.ClassName}
-                <br />
-                <b>תאריך:</b>{" "}
-                {new Date(classToCard.ClassDate).toLocaleDateString("en-GB")}{" "}
-                <br />
-                <b>שעת התחלה:</b> {classToCard.StartTime}
-              </Col>
-            </Row>
-          </Card.Body>
-        </Card>
-      </>
-    );
-  } else {
-    return (
-      <>
-        <Card>
-          <Card.Body>
-            <Row>
-              <Col xs={2}>
-                <Button variant="secondary">עדכון</Button>
-              </Col>
-              <Col xs={2}>
-                <Button
-                  onClick={() => ShowModaAreYouSure(classToCard)}
-                  variant="outline-danger"
-                >
-                  מחיקה
-                </Button>
-              </Col>
-              <Col xs={8}>
-                <b>שם השיעור:</b> {classToCard.ClassName}
-                <br />
-                <b>תאריך:</b>{" "}
-                {new Date(classToCard.ClassDate).toLocaleDateString("en-GB")}{" "}
-                <br />
-                <b>שעת התחלה:</b> {classToCard.StartTime}
-              </Col>
-            </Row>
-          </Card.Body>
-        </Card>
-      </>
-    );
+  const Navigate = useNavigate();
+
+  switch (type) {
+    case "history":
+      return (
+        <>
+          <Card>
+            <Card.Body>
+              <Row>
+                <Col xs={12} style={{ display: "block", textAlign: "right" }}>
+                  <b>שם השיעור:</b> {classToCard.ClassName}
+                  <br />
+                  <b>תאריך:</b>{" "}
+                  {new Date(classToCard.ClassDate).toLocaleDateString("en-GB")}{" "}
+                  <br />
+                  <b>שעת התחלה:</b> {classToCard.StartTime}
+                  <br />
+                  <Button
+                    size="sm"
+                    onClick={() =>
+                      Navigate("/FCFormSuperDetails", {
+                        state: classToCard.SuperStudentId,
+                      })
+                    }
+                  >
+                    {classToCard.SuperName}
+                  </Button>
+                </Col>
+              </Row>
+            </Card.Body>
+          </Card>
+        </>
+      );
+    case "studentFutre":
+      return (
+        <>
+          <Card>
+            <Card.Body>
+              <Row className="d-flex align-items-center">
+                <Col xs={2}>
+                  <Button
+                    size="md"
+                    onClick={() => ShowModaAreYouSure(classToCard)}
+                    variant="outline-danger"
+                  >
+                    מחיקה
+                  </Button>
+                </Col>
+                <Col xs={2}>
+                  <Button
+                    size="md"
+                    onClick={() => ShowModaAreYouSure(classToCard)}
+                    variant="outline-primary"
+                  >
+                    לצ'אט
+                  </Button>
+                </Col>
+                <Col xs={8} style={{ display: "block", textAlign: "right" }}>
+                  <b>שם השיעור:</b> {classToCard.ClassName}
+                  <br />
+                  <b>תאריך:</b>{" "}
+                  {new Date(classToCard.ClassDate).toLocaleDateString("en-GB")}{" "}
+                  <br />
+                  <b>שעת התחלה:</b> {classToCard.StartTime}
+                  <br />
+                  <Button
+                    size="sm"
+                    onClick={() => {
+                      Navigate("/FCFormSuperDetails", {
+                        state: {
+                          id:classToCard.SuperStudentId
+                        }
+                      });
+                      console.log(classToCard.SuperStudentId);
+                    }}
+                  >
+                    {classToCard.SuperName}
+                  </Button>
+                </Col>
+              </Row>
+            </Card.Body>
+          </Card>
+        </>
+      );
+    default:
+      return (
+        <>
+          <Card>
+            <Card.Body>
+              <Row>
+                <Col xs={2}>
+                  <Button variant="secondary">עדכון</Button>
+                </Col>
+                <Col xs={2}>
+                  <Button
+                    onClick={() => ShowModaAreYouSure(classToCard)}
+                    variant="outline-danger"
+                  >
+                    מחיקה
+                  </Button>
+                </Col>
+                <Col xs={8}>
+                  <b>שם השיעור:</b> {classToCard.ClassName}
+                  <br />
+                  <b>תאריך:</b>{" "}
+                  {new Date(classToCard.ClassDate).toLocaleDateString("en-GB")}{" "}
+                  <br />
+                  <b>שעת התחלה:</b> {classToCard.StartTime}
+                </Col>
+              </Row>
+            </Card.Body>
+          </Card>
+        </>
+      );
   }
+  // if (type === "history") {
+  //   return (
+  //     <>
+  //       <Card>
+  //         <Card.Body>
+  //           <Row>
+  //             <Col xs={12}>
+  //               <b>שם השיעור:</b> {classToCard.ClassName}
+  //               <br />
+  //               <b>תאריך:</b>{" "}
+  //               {new Date(classToCard.ClassDate).toLocaleDateString("en-GB")}{" "}
+  //               <br />
+  //               <b>שעת התחלה:</b> {classToCard.StartTime}
+  //             </Col>
+  //           </Row>
+  //         </Card.Body>
+  //       </Card>
+  //     </>
+  //   );
+  // } else if (type === "studentFutre") {
+  //   return (
+  //     <>
+  //       <Card>
+  //         <Card.Body>
+  //           <Row>
+  //             <Col xs={2}>
+  //               <Button
+  //                 onClick={() => ShowModaAreYouSure(classToCard)}
+  //                 variant="outline-danger"
+  //               >
+  //                 מחיקה
+  //               </Button>
+  //             </Col>
+  //             <Col xs={2}>
+  //               <Button
+  //                 onClick={() => ShowModaAreYouSure(classToCard)}
+  //                 variant="outline-primary"
+  //               >
+  //                 לצ'אט
+  //               </Button>
+  //             </Col>
+  //             <Col xs={8}>
+  //               <b>שם השיעור:</b> {classToCard.ClassName}
+  //               <br />
+  //               <b>תאריך:</b>{" "}
+  //               {new Date(classToCard.ClassDate).toLocaleDateString("en-GB")}{" "}
+  //               <br />
+  //               <b>שעת התחלה:</b> {classToCard.StartTime}
+  //             </Col>
+  //           </Row>
+  //         </Card.Body>
+  //       </Card>
+  //     </>
+  //   );
+  // } else {
+  //   return (
+  //     <>
+  //       <Card>
+  //         <Card.Body>
+  //           <Row>
+  //             <Col xs={2}>
+  //               <Button variant="secondary">עדכון</Button>
+  //             </Col>
+  //             <Col xs={2}>
+  //               <Button
+  //                 onClick={() => ShowModaAreYouSure(classToCard)}
+  //                 variant="outline-danger"
+  //               >
+  //                 מחיקה
+  //               </Button>
+  //             </Col>
+  //             <Col xs={8}>
+  //               <b>שם השיעור:</b> {classToCard.ClassName}
+  //               <br />
+  //               <b>תאריך:</b>{" "}
+  //               {new Date(classToCard.ClassDate).toLocaleDateString("en-GB")}{" "}
+  //               <br />
+  //               <b>שעת התחלה:</b> {classToCard.StartTime}
+  //             </Col>
+  //           </Row>
+  //         </Card.Body>
+  //       </Card>
+  //     </>
+  //   );
+  // }
 }
