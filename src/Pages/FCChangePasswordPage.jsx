d3063ecdba00e4e1ee9dd3d60163334981d2664b
nemir313 <76554952+nemir313@users.noreply.github.com>
2022-05-23 23:16:12 +0300

    שינוי סיסמה - לא גמור

--- src/Pages/FCChangePasswordPage.jsx
+++ src/Pages/FCChangePasswordPage.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { useState } from "react";
 import { Container, Card, Form, Button } from "react-bootstrap";
 
 export default function FCChangePasswordPage() {
+
+  const [id, setId] = useState("")
+  const fetchNewPass = () => {
+    const newPassUrl = "http://localhost:49812/Student/NewPassword/"
+    console.log("start")
+    fetch(newPassUrl + id, {
+      method: "POST",
+      headers: new Headers({
+        "Content-Type": "application/json; charset=UTF-8",
+        Accept: "application/json; charset=UTF-8",
+      }),
+    })
+      .then((res) => {
+        console.log("res=", res);
+        console.log("res.status", res.status);
+        console.log("res.ok", res.ok);
+        return res.json();
+      })
+      .then(
+        (result) => {
+          console.log("FETCH Post Email = ", result);
+        },
+        (error) => {
+          console.log("err post =", error);
+        }
+      );
+    console.log("end")
+  }
   return (
     <Container className="d-flex justify-content-start align-items-center flex-column ">
       <img
@@ -23,18 +52,19 @@ export default function FCChangePasswordPage() {
                 className="mb-2"
                 type="text"
                 required
+                onChange={(e) => setId(e.target.value)}
               />
             </Form.Group>
             <Button
             className="w-100 text-center mt-2"
             variant="success"
+            onClick={() => fetchNewPass()}
           >
             שלחו אליי סיסמה חדשה
           </Button>
           </Form>
         </Card.Body>
       </Card>
-    
     </Container>
   );
 }
